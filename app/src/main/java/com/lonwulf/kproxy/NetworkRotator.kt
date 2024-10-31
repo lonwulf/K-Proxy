a37f3d5ab365105761de11bd340f18142c80c61e
kenkiwa <kenkiwa.kw@gmail.com>
2024-10-31 16:17:56 +0300

    comprehensive error handling

--- app/src/main/java/com/lonwulf/kproxy/NetworkRotator.kt
+++ app/src/main/java/com/lonwulf/kproxy/NetworkRotator.kt
@@ -1,7 +1,9 @@
 package com.lonwulf.kproxy
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
@@ -9,6 +11,7 @@ import android.net.NetworkRequest
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.provider.Settings
+import androidx.core.content.ContextCompat
 import kotlinx.coroutines.delay
 import timber.log.Timber
 import javax.inject.Inject
@@ -29,39 +32,54 @@ class NetworkRotator @Inject constructor(private val ctx: Context) {
     }
 
     private fun toggleConnection(enable: Boolean) {
-        when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
-                try {
-                    val settingsIntent = Intent(Settings.Panel.ACTION_WIFI).apply {
+        try {
+            toggleMobileData(enable)
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+
+                    val settingsIntent = Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY).apply {
                         addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                     }
                     ctx.startActivity(settingsIntent)
-                } catch (e: Exception) {
-                    Timber.e(e, "Failed to open connectivity settings panel")
+                }
+
+                else -> {
+                    @Suppress("DEPRECATION")
+                    wifiManager.isWifiEnabled = enable
                 }
             }
-
-            else -> {
-                @Suppress("DEPRECATION")
-                wifiManager.isWifiEnabled = enable
-            }
+        } catch (e: Exception) {
+            Timber.e(e, "Failed to toggle connection")
         }
     }
 
     private fun toggleMobileData(enable: Boolean) {
-        val request = NetworkRequest.Builder()
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-            .build()
+        try {
+            if (ContextCompat.checkSelfPermission(
+                    ctx,
+                    Manifest.permission.CHANGE_NETWORK_STATE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                Timber.w("Missing MODIFY_PHONE_STATE permission")
+                return
+            }
+            val request = NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .build()
 
-        if (enable) {
-            cM.registerNetworkCallback(request, object : ConnectivityManager.NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                    cM.bindProcessToNetwork(network)
-                }
-            })
-        } else {
-            cM.unregisterNetworkCallback(ConnectivityManager.NetworkCallback())
+            if (enable) {
+                cM.registerNetworkCallback(request, object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        cM.bindProcessToNetwork(network)
+                    }
+                })
+            } else {
+                cM.unregisterNetworkCallback(ConnectivityManager.NetworkCallback())
+            }
+        } catch (e: Exception) {
+            Timber.e(e, "Failed to toggle mobile data")
         }
+
     }
 }
